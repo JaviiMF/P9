e7ef795cd168898c72ee821be9ad8be149a2c096
JaviiMF <jmf64@gcloud.ua.es>
2023-12-27 15:43:08 +0100

    Modificamos la primera version sin fallos

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def suma(a,b):
 	return a+b
 
-resultado = suma(4,9)
-print("El resultado de la suma es: ", resultado)
+resultado_1 = suma(4,9)
+resultado_2 = suma(12,6)
+
+print("El resultado de la primera suma es: ", resultado_1)
+print("El resultado de la segunda suma es: ", resultado_2)
