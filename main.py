de30b0df1754986e0c183cedf33fae670d5bcc7e
JaviiMF <jmf64@gcloud.ua.es>
2023-12-27 15:51:03 +0100

    Añado una ultima operacion

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def suma(a,b):
 
 resultado_1 = suma(4,9)
 resultado_2 = suma(12,6)
+resultado_3 = suma(15,15)
 
 print("El resultado de la primera suma es: ", resultado_1)
 print("El resultado de la segunda suma es: ", resultado_2)
+print("El resultado de la tercera suma es: ", resultado_3)
